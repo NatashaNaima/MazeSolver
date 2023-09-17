3d503032a7b8f587c764307a085133504090cff6
Natasha <natasha.naima.hussain@gmail.com>
2023-09-16 21:43:07 -0400

    added break-wall method

--- maze.py
+++ maze.py
@@ -23,25 +23,25 @@ class Maze:
         self.create_cells()
 
     def create_cells(self):
-        for i in range(self.num_cols):
-            col_cells = []
-            for j in range(self.num_rows):
-                col_cells.append(Cell(self.win))
-            self.cells.append(col_cells)
+        for row in range(self.num_rows):
+            row_cells = []
+            for col in range(self.num_cols):
+                row_cells.append(Cell(self.win))
+            self.cells.append(row_cells)
         
-        for i in range(self.num_cols):
-            for j in range(self.num_rows):
-                self.draw_cell(i,j)
+        for row in range(self.num_rows):
+            for col in range(self.num_cols):
+                self.draw_cell(row,col)
         
 
-    def draw_cell(self, i, j):
+    def draw_cell(self, row, col):
         if self.win is None:
             return
         
-        x1 = self.topLeftCorner.x + i * self.cell_size
-        y1 = self.topLeftCorner.y + j * self.cell_size
+        x1 = self.topLeftCorner.x + col * self.cell_size
+        y1 = self.topLeftCorner.y + row * self.cell_size
 
-        self.cells[i][j].draw(x1, y1, self.cell_size)
+        self.cells[row][col].draw(x1, y1, self.cell_size)
         self.animate()
 
     def animate(self):
@@ -50,4 +50,29 @@ class Maze:
         self.win.redraw()
         time.sleep(0.05)
     
+    def break_entrance_and_exit(self):
+        pass
+
+    def break_wall(self, row, col, wall):
+        cell = self.cells[row][col]
+
+        if wall == 'left':
+            cell.has_left_wall = False
+            if col not in range(1,self.num_cols):
+                self.cells[row][col-1].has_right_wall = False
+
+        if wall == 'top':
+            cell.has_top_wall = False
+            if row not in range(1, self.num_rows):
+                self.cells[row-1][col].has_bottom_wall = False
+
+        if wall == 'right':
+            cell.has_right_wall = False
+            if col not in range(1,self.num_cols):
+                self.cells[row][col+1].has_left_wall = False
+
+        if wall == 'bottom':
+            cell.has_bottom_wall = False
+            if row not in range(0, self.num_rows-1):
+                self.cells[row+1][col].has_top_wall = False 
 
